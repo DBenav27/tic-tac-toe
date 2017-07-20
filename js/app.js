fbc0328bb174e51d8aa4ef2597e09b507831a631
Daniel Benavidez <danielbenavidez27@gmail.com>
2017-07-19 23:17:59 -0500

    redoing tic tac toe

--- js/app.js
+++ js/app.js
@@ -11,7 +11,7 @@ $(document).ready(function() {
 
   function winConditions() {
 
-    //For Vertical Wins
+//For Vertical Wins
     if (($('#box1').hasClass('X') && $('#box2').hasClass('X')) && $('#box3').hasClass('X')) {
       alert('X wins!');
     } else if (($('#box1').hasClass('O') && $('#box2').hasClass('O')) && $('#box3').hasClass('O')) {
@@ -20,47 +20,39 @@ $(document).ready(function() {
       alert('X wins!');
     } else if (($('#box4').hasClass('O') && $('#box5').hasClass('O')) && $('#box6').hasClass('O')) {
       alert('O wins!');
-    } else if (($('#box7').hasClass('X')&& $('#box8').hasClass('X')) && $('#box9').hasClass('X')) {
+    } else if (($('#box7').hasClass('X') && $('#box8').hasClass('X')) && $('#box9').hasClass('X')) {
       alert('X wins!');
     } else if (($('#box7').hasClass('O') && $('#box8').hasClass('O')) && $('#box9').hasClass('O')) {
       alert('O wins!');
-      //For Horizonal Wins
-    } else if (($('#box1').hasClass('X')&& $('#box4').hasClass('X')) && $('#box7').hasClass('X')) {
+//For Horizonal Wins
+    } else if (($('#box1').hasClass('X') && $('#box4').hasClass('X')) && $('#box7').hasClass('X')) {
       alert('X wins!');
     } else if (($('#box1').hasClass('O') && $('#box4').hasClass('O')) && $('#box7').hasClass('O')) {
       alert('O wins!');
-    } else if (($('#box2').hasClass('X')&& $('#box5').hasClass('X')) && $('#box8').hasClass('X')) {
+    } else if (($('#box2').hasClass('X') && $('#box5').hasClass('X')) && $('#box8').hasClass('X')) {
       alert('X wins!');
     } else if (($('#box2').hasClass('O') && $('#box5').hasClass('O')) && $('#box8').hasClass('O')) {
       alert('O wins!');
-    } else if (($('#box3').hasClass('X')&& $('#box6').hasClass('X')) && $('#box9').hasClass('X')) {
+    } else if (($('#box3').hasClass('X') && $('#box6').hasClass('X')) && $('#box9').hasClass('X')) {
       alert('X wins!');
     } else if (($('#box3').hasClass('O') && $('#box6').hasClass('O')) && $('#box9').hasClass('O')) {
       alert('O wins!');
-      //Diagonal Wins
-    } else if (($('#box1').hasClass('X')&& $('#box5').hasClass('X')) && $('#box9').hasClass('X')) {
+//Diagonal Wins
+    } else if (($('#box1').hasClass('X') && $('#box5').hasClass('X')) && $('#box9').hasClass('X')) {
       alert('X wins!');
     } else if (($('#box1').hasClass('O') && $('#box5').hasClass('O')) && $('#box9').hasClass('O')) {
       alert('O wins!');
-    } else if (($('#box3').hasClass('X')&& $('#box5').hasClass('X')) && $('#box7').hasClass('X')) {
+    } else if (($('#box3').hasClass('X') && $('#box5').hasClass('X')) && $('#box7').hasClass('X')) {
       alert('X wins!');
     } else if (($('#box3').hasClass('O') && $('#box5').hasClass('O')) && $('#box7').hasClass('O')) {
       alert('O wins!');
     }
   }
 
-  function switchPlayer() {
-    if (currentPlayer === "X") {
-      currentPlayer = secondPlayer;
-    } else {
-      currentPlayer = firstPlayer;
-    }
-  }
-
-  // click function
+//Click Function
   $('.col-md-4').click(function() {
     console.log(currentPlayer);
-    //box is claimed
+//Box is claimed
     if ($(this).hasClass('X') || $(this).hasClass('O')) {
       alert("This tile is already filled!");
     } else {
@@ -68,10 +60,18 @@ $(document).ready(function() {
       switchPlayer();
       winConditions();
     }
+    //Switching Player
+      function switchPlayer() {
+        if (currentPlayer === "X") {
+          currentPlayer = secondPlayer;
+        } else {
+          currentPlayer = firstPlayer;
+        }
+      }
 
 
   });
-
+//Reset moves
   $('.reset').click(function() {
     $('.col-md-4').removeClass("X");
     $('.col-md-4').removeClass("O");
